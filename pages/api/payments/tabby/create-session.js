6bd0d9cb8e47730cc065610fcfea7ce90ce7f220
Shade1269 <162415959+Shade1269@users.noreply.github.com>
2025-08-29 00:16:22 -0700

    Create create-session.js

---
+++ pages/api/payments/tabby/create-session.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  // TODO: Integrate with Tabby to create a payment session
+  console.log('Creating Tabby session...', req.body);
+  return res.status(200).json({ sessionUrl: '' });
+}
